e4bd887f877a51bbc73b73da1f6ba06f9b85f2bb
Олег <bonbarcovoleg55@gmail.com>
2023-12-24 18:30:35 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -155,51 +155,7 @@ $(document).ready(function() {
 	
 	
 
-      {
-            label: "",
-            fillColor: "transparent",
-            strokeColor: "#ed6c3b",
-            pointColor: "#ed6c3b",
-            pointStrokeColor: "#ed6c3b",
-            pointHighlightFill: "#ed6c3b",
-            pointHighlightStroke: "#ed6c3b",
-            data: [0, 0, 10, 50, 70, 90, 210, 230, 150, 184, 200, 150]
-        }
-				  
-				  
-				  ]
-    };
-    var lineGraphOptions = {
-
-        ///Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines: true,
-
-        //String - Colour of the grid lines
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-
-        //Number - Width of the grid lines
-        scaleGridLineWidth: 1,
-
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
-
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines: true,
-
-        //Boolean - Whether the line is curved between points
-        bezierCurve: false,
-
-        //Number - Tension of the bezier curve between points
-        bezierCurveTension: 0.4,
-
-        //Boolean - Whether to show a dot for each point
-        pointDot: true,
-
-        //Number - Radius of each point dot in pixels
-        pointDotRadius: 4,
-
-        //Number - Pixel width of point dot stroke
-        pointDotStrokeWidth: 1,
+    : 1,
 
         //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
         pointHitDetectionRadius: 20,
